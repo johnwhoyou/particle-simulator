edb3429956ec54cd142734dc105eed1f7c425034
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2024-02-07 17:12:07 +0800

    changes to particle and wall interaction

--- include/Particle.h
+++ include/Particle.h
@@ -1,12 +1,14 @@
 #ifndef PARTICLE_H
 #define PARTICLE_H
 
+#include "Wall.h"
+
 class Particle {
 public:
     Particle(float x, float y, float angle, float velocity);
 
     void update(float deltaTime);
-    void bounceOffWall(float wallAngle);
+    void bounceOffWall(const Wall& wall);
 
     float getX() const;
     float getY() const;
@@ -16,8 +18,9 @@ private:
     float x, y; // Position of the particle
     float angle; // Angle in degrees, 0 is east
     float velocity; // Velocity in pixels per second
-
+    
     void move(float deltaTime);
+    bool isParticleOnRightSideOfWall(const Wall& wall) const;
 };
 
 #endif
--- src/Particle.cpp
+++ src/Particle.cpp
@@ -23,10 +23,44 @@ void Particle::move(float deltaTime) {
     y += std::sin(degToRad(angle)) * velocity * deltaTime;
 }
 
-void Particle::bounceOffWall(float wallAngle) {
-    // Reflect the movement angle based on the wall's angle
-    angle = 2 * wallAngle - angle;
+void Particle::bounceOffWall(const Wall& wall) {
+    float wallAngle = wall.getAngle();  // angle of the wall with respect to the positive x-axis
+
+    // check if the wall is vertical (east or west wall)
+    if (wallAngle == 0.0f || wallAngle == 180.0f) {
+        // if vertical wall, simply mirror the horizontal component of the angle
+        angle = 360.0f - angle;
+    } else if (wallAngle == 90.0f || wallAngle == 270.0f) {
+        // if horizontal wall (north or south wall), mirror the vertical component
+        angle = 180.0f - angle;
+    } else {
+        // for walls with arbitrary angles, use the previous reflection logic
+        float normalAngle;
+        if (isParticleOnRightSideOfWall(wall)) {
+            normalAngle = normalizeAngle(wallAngle + 90.0f);
+        } else {
+            normalAngle = normalizeAngle(wallAngle - 90.0f);
+        }
+        float angleOfIncidence = normalizeAngle(angle - normalAngle);
+        angle = normalizeAngle(normalAngle - angleOfIncidence);
+    }
+
+    // normalize the final angle
     angle = normalizeAngle(angle);
+}
+
+bool Particle::isParticleOnRightSideOfWall(const Wall& wall) const {
+    // use the cross product to determine the relative position
+    // compute wall vector
+    float wallVecX = wall.getX2() - wall.getX1();
+    float wallVecY = wall.getY2() - wall.getY1();
+    // compute particle vector
+    float particleVecX = x - wall.getX1();
+    float particleVecY = y - wall.getY1();
+    // cross product to determine the side
+    float crossProduct = (wallVecX * particleVecY) - (wallVecY * particleVecX);
+    
+    return crossProduct > 0; // if cross product is positive, particle is on right side
 }
 
 float Particle::getX() const {
